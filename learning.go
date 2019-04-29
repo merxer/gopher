5cfbdf31ea434d30ed9e4086a7efdac4d125cdd8
THAWATCHAI SINGNGAM <merxer.io@gmail.com>
2019-04-29 14:53:20 +0700

    delete unused file

--- learning.go
+++ learning.go
@@ -1,9 +1,11 @@
-package main 
+package main
 
 import "fmt"
 
-var name string = "Pat"
-var date int = 29
+var (
+	name string = "Pat"
+	date int    = 29
+)
 
 func main() {
 	fmt.Println(name, date)
